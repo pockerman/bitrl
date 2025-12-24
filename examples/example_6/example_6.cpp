75e22543c0e3586490fe6edb61d3be33e7b7117d
pockerman <a.giavaras@gmail.com>
2025-12-24 10:39:52 +0000

    Update example 6

--- examples/example_6/example_6.cpp
+++ examples/example_6/example_6.cpp
@@ -1,5 +1,7 @@
 
-#include "bitrl/envs/gymnasium/classic_control/pendulum_env.h"
+//#include "bitrl/envs/gymnasium/classic_control/pendulum_env.h"
+//#include "bitrl/envs/gymnasium/classic_control/vector/acrobot_vec_env.h"
+#include "bitrl/envs/gymnasium/classic_control/cart_pole_env.h"
 #include "bitrl/network/rest_rl_env_client.h"
 #include "bitrl/bitrl_types.h"
 #include "bitrl/bitrl_consts.h"
@@ -14,42 +16,91 @@
 #include <random>
 #include <unordered_map>
 
- 
+namespace example
+{
+	using namespace bitrl;
+	using namespace bitrl::envs::gymnasium;
+	using bitrl::network::RESTRLEnvClient;
+
+	void test_cart_pole(RESTRLEnvClient& server)
+	{
+		// the environment is not registered with the server
+		std::cout<<"Is environment registered: "<<server.is_env_registered(CartPole::name)<<std::endl;
+
+		// create the environment
+		CartPole env(server);
+
+		std::cout<<"Name: "<<env.name<<std::endl;
+		std::cout<<"Number of actions: "<<env.n_actions()<<std::endl;
+
+		// make the environment
+		std::unordered_map<std::string, std::any> options;
+		std::unordered_map<std::string, std::any> reset_ops;
+		reset_ops.insert({"seed", static_cast<uint_t>(42)});
+		env.make("v1", options, reset_ops);
+
+		auto time_step = env.reset();
+		std::cout<<"Time step: "<<time_step<<std::endl;
+
+		// step in the environment
+		time_step = env.step(0);
+		std::cout<<"Time step after action: "<<time_step<<std::endl;
+
+		env.close();
+	}
+	// void test_pendulum(RESTRLEnvClient& server)
+	// {
+	//
+	// 	// the environment is not registered with the server
+	// 	std::cout<<"Is environment registered: "<<server.is_env_registered(Pendulum::name)<<std::endl;
+	//
+	// 	// create the environment
+	// 	Pendulum env(server);
+	//
+	// 	std::cout<<"Name: "<<env.name<<std::endl;
+	// 	std::cout<<"Number of actions: "<<env.n_actions()<<std::endl;
+	//
+	// 	// make the environment
+	// 	std::unordered_map<std::string, std::any> options;
+	// 	std::unordered_map<std::string, std::any> reset_ops;
+	// 	reset_ops.insert({"seed", static_cast<uint_t>(42)});
+	// 	env.make("v1", options, reset_ops);
+	//
+	// 	auto time_step = env.reset();
+	// 	std::cout<<"Time step: "<<time_step<<std::endl;
+	//
+	// 	// step in the environment
+	// 	time_step = env.step(0.0);
+	// 	std::cout<<"Time step after action: "<<time_step<<std::endl;
+	//
+	// 	env.close();
+	// }
+
+}
 
 
 int main(){
 
-	using namespace bitrl;
-	using namespace bitrl::envs::gymnasium;
-	using bitrl::network::RESTRLEnvClient;
+	using namespace  example;
 	
 	const std::string SERVER_URL = "http://0.0.0.0:8001/api";
     
 	RESTRLEnvClient server(SERVER_URL, true);
+
+	std::cout<<"Testing FrozenLake..."<<std::endl;
+	example::test_cart_pole(server);
+	std::cout<<"===================="<<std::endl;
+	std::cout<<"Testing Taxi..."<<std::endl;
+	//example::test_pendulum(server);
+	std::cout<<"===================="<<std::endl;
+	// std::cout<<"Testing BlackJack..."<<std::endl;
+	// example_1::test_black_jack(server);
+	// std::cout<<"===================="<<std::endl;
+	// std::cout<<"Testing CliffWorld..."<<std::endl;
+	// example_1::test_cliff_world(server);
+	// std::cout<<"===================="<<std::endl;
+	return 0;
 	
-	Pendulum env(server);
-	
-	std::cout<<"Name: "<<env.name<<std::endl;
-	std::cout<<"Number of actions: "<<env.n_actions()<<std::endl;
-	
-	// make the environment
-	std::unordered_map<std::string, std::any> options;
-	std::unordered_map<std::string, std::any> reset_ops;
-	reset_ops.insert({"seed", static_cast<uint_t>(42)});
-	env.make("v1", options, reset_ops);
-	
-	auto time_step = env.reset();
-	
-	std::cout<<"Time step: "<<time_step<<std::endl;
-	
-	// step in the environment
-	time_step = env.step(0.0);
-	
-	std::cout<<"Time step after action: "<<time_step<<std::endl;
-	
-	env.close();
-	
-	std::cout<<"Is active? "<<env.is_alive()<<std::endl;
-	
+
     return 0;
 }
